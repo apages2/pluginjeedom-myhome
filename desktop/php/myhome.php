c26c6a8a7362677b61bb49127ac4f5d24fe88c38
root <root@jeedom-preprod.(none)>
2017-03-31 13:34:28 +0200

    gestion des onglets dans equipements

--- desktop/php/myhome.php
+++ desktop/php/myhome.php
@@ -2,18 +2,18 @@
 if (!isConnect('admin')) {
     throw new Exception('Error 401 Unauthorized');
 }
-sendVarToJS('eqType', 'myhome');
-$eqLogics = eqLogic::byType('myhome');
+$plugin = plugin::byId('myhome');
+sendVarToJS('eqType', $plugin->getId());
+$eqLogics = eqLogic::byType($plugin->getId());
+function sortByOption($a, $b) {
+	return strcmp($a['name'], $b['name']);
+}
 if (config::byKey('autoDiscoverEqLogic', 'myhome', 0) == 1) {
 	echo '<div class="alert jqAlert alert-warning" id="div_inclusionAlert" style="margin : 0px 5px 15px 15px; padding : 7px 35px 7px 15px;">{{Vous etes en mode inclusion. Recliquez sur le bouton d\'inclusion pour sortir de ce mode}}</div>';
 } else {
 	echo '<div id="div_inclusionAlert"></div>';
 }
 ?>
-
-<script type="text/javascript">
-
-</script>
 
 <div class="row row-overflow">
     <div class="col-lg-2 col-md-3 col-sm-4">
@@ -34,8 +34,10 @@ if (config::byKey('autoDiscoverEqLogic', 'myhome', 0) == 1) {
 	<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
 		<legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
 		<div class="eqLogicThumbnailContainer">
-			<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" ><center>
-				<i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i></center>
+			<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+				<center>
+					<i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
+				</center>
 				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
 			</div>
 			<?php
@@ -55,8 +57,10 @@ if (config::byKey('autoDiscoverEqLogic', 'myhome', 0) == 1) {
 					echo '</div>';
 				}
 			?>
-			<div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;"><center>
-				<i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i></center>
+			<div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+				<center>
+					<i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+				</center>
 				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
 			</div>
 		</div>
@@ -69,10 +73,13 @@ if (config::byKey('autoDiscoverEqLogic', 'myhome', 0) == 1) {
 				$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 				echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
 				echo "<center>";
-				if (file_exists(dirname(__FILE__) . '/../../core/config/devices/' . $device_id . '.jpg')) {
-					echo '<img class="lazy" src="plugins/myhome/core/config/devices/' . $device_id . '.jpg" height="105" width="95" />';
+				$alternateImg = $eqLogic->getConfiguration('iconModel');
+				if (file_exists(dirname(__FILE__) . '/../../core/config/devices/' . $alternateImg . '.jpg')) {
+					echo '<img class="lazy" src="plugins/myhome/core/config/devices/' . $alternateImg . '.jpg" height="105" width="95" />';
+				}elseif (file_exists(dirname(__FILE__) . '/../../core/config/devices/' . $eqLogic->getConfiguration('device') . '.jpg')) {
+					echo '<img class="lazy" src="plugins/myhome/core/config/devices/' . $eqLogic->getConfiguration('device') . '.jpg" height="105" width="95" />';
 				} else {
-					echo '<img class="lazy" src="plugins/myhome/doc/images/myhome_icon.png" height="105" width="95" />';
+					echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
 				}
 				echo "</center>";
 				echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
@@ -80,26 +87,34 @@ if (config::byKey('autoDiscoverEqLogic', 'myhome', 0) == 1) {
 			}
 			?>
 		</div>
-		</div>
+	</div>
 
     <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-        <div class="row">
+        <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+		<a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+		<a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
+		<a class="btn btn-default eqLogicAction pull-right" data-action="copy"><i class="fa fa-files-o"></i> {{Dupliquer}}</a>
+		<ul class="nav nav-tabs" role="tablist">
+			<li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+			<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+			<li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+		</ul>
+		<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+		<div role="tabpanel" class="tab-pane active" id="eqlogictab">
+		<br/>
+		<div class="row">
             <div class="col-sm-6">
                 <form class="form-horizontal">
                     <fieldset>
-                        <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}
-                            <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i>
-                            <a class="btn btn-xs btn-default pull-right eqLogicAction" data-action="copy"><i class="fa fa-files-o"></i> {{Dupliquer}}</a>
-                        </legend>
                         <div class="form-group">
-                            <label class="col-sm-3 control-label">Nom de l'équipement Myhome</label>
+                            <label class="col-sm-3 control-label">{{Nom de l'équipement Myhome}}</label>
                             <div class="col-sm-4">
                                 <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
 								<input type="text" class="eqLogicAttr form-control" data-l1key="name" style="padding-left:12px" placeholder="Nom de l'équipement Myhome"/>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-sm-3 control-label">ID (HEXA/DECIMAL)</label>
+                            <label class="col-sm-3 control-label">{{ID (HEXA/DECIMAL)}}</label>
                             <div class="col-sm-2">
                                 <input type="text" id="myhomeid" class="eqLogicAttr form-control" data-l1key="logicalId" style="padding-left:12px" placeholder="Logical ID" />
 							</div>
@@ -109,13 +124,13 @@ if (config::byKey('autoDiscoverEqLogic', 'myhome', 0) == 1) {
                         </div>
 						<div class="form-group">
                             <label class="col-sm-3 control-label"></label>
-                            <div class="col-sm-8">
-								<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
-								<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/> 
-                            </div>
+                            <div class="col-sm-9">
+								<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+								<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+							</div>
                         </div>
                         <div class="form-group">
-                            <label class="col-sm-3 control-label" >Objet parent</label>
+                            <label class="col-sm-3 control-label" >{{Objet parent}}</label>
                             <div class="col-sm-4">
                                 <select class="eqLogicAttr form-control" data-l1key="object_id">
                                     <option value="">Aucun</option>
@@ -128,7 +143,7 @@ if (config::byKey('autoDiscoverEqLogic', 'myhome', 0) == 1) {
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-sm-3 control-label">Catégorie</label>
+                            <label class="col-sm-3 control-label">{{Catégorie}}</label>
                             <div class="col-sm-9">
                                 <?php
                                 foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
@@ -152,7 +167,7 @@ if (config::byKey('autoDiscoverEqLogic', 'myhome', 0) == 1) {
             <div class="col-sm-6">
                 <form class="form-horizontal">
                     <fieldset>
-                        <legend><i class="fa fa-info-circle"></i>  Informations</legend>
+                        <legend><i class="fa fa-info-circle"></i>{{Informations}}</legend>
                          <div id="div_instruction"></div>
 						 <div class="form-group expertModeVisible">
                             <label class="col-sm-3 control-label">{{Création}}</label>
@@ -165,8 +180,8 @@ if (config::byKey('autoDiscoverEqLogic', 'myhome', 0) == 1) {
 							</div>
                         </div>
                         <div class="form-group">
-                            <label class="col-sm-3 control-label">Equipement</label>
-                            <div class="col-sm-6">
+                            <label class="col-sm-3 control-label">{{Equipement}}</label>
+                            <div class="col-sm-9">
                                 <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="device">
                                     <option value="">Aucun</option>
                                     <?php
@@ -180,40 +195,40 @@ if (config::byKey('autoDiscoverEqLogic', 'myhome', 0) == 1) {
                             </div>
                         </div>
 						<div class="form-group">
-								<label class="col-sm-3 control-label">Version Utilisée du template</label>
+								<label class="col-sm-3 control-label">{{Version Utilisée du template}}</label>
 								<div class="col-sm-2">
 									<span id="vinst" class="eqLogicAttr label label-default tooltips" data-l1key="configuration" data-l2key="version" style="font-size : 1em;"></span>
 								</div>
-								<label class="col-sm-4 control-label">Version Disponible du template</label>
+								<label class="col-sm-4 control-label">{{Version Disponible du template}}</label>
 								<div class="col-sm-3">
 									<span id="vdispo" class="eqLogicAttr label label-default tooltips" style="font-size : 1em;"></span>
 								</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">{{Release Notes du template}}</label>
+							<div class="col-sm-9">
+								<span id="rnotes" class="eqLogicAttr label label-default tooltips" style="font-size : 1em;"></span>
 							</div>
-							<div class="form-group">
-								<label class="col-sm-3 control-label">Release Notes du template</label>
-								<div class="col-sm-3">
-									<span id="rnotes" class="eqLogicAttr label label-default tooltips" style="font-size : 1em;"></span>
-								</div>
-							</div>
-                    </fieldset> 
-                </form>
-            </div>
+						</div>
+			        </fieldset> 
+				</form>
+			</div>
         </div>
+        </div>
+        <div role="tabpanel" class="tab-pane" id="commandtab">
 
-        <legend><i class="fa fa-list-alt"></i>  Commandes</legend>
 
-
-        <a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> Ajouter une commande</a><br/><br/>
+        <a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une commande}}</a><br/><br/>
         <table id="table_cmd" class="table table-bordered table-condensed">
             <thead>
                 <tr>
-                    <th style="width: 300px;">Nom</th>
-                    <th style="width: 130px;" class="expertModeVisible">Type</th>
-					<th style="width: 70px;" class="expertModeVisible">Unit</th>
-					<th style="width: 130px;" class="expertModeVisible">Communication</th>
-                    <th class="expertModeVisible">Logical ID (info) ou Commande brute (action)</th>
-                    <th>Paramètres</th>
-                    <th style="width: 100px;">Options</th>
+                    <th style="width: 300px;">{{Nom</th>
+                    <th style="width: 130px;" class="expertModeVisible">{{Type}}</th>
+					<th style="width: 70px;" class="expertModeVisible">{{Unit}}</th>
+					<th style="width: 130px;" class="expertModeVisible">{{Communication}}</th>
+                    <th class="expertModeVisible">{{Logical ID (info) ou Commande brute (action)}}</th>
+                    <th>{{Paramètres}}</th>
+                    <th style="width: 100px;">{{Options}}</th>
                     <th></th>
                 </tr>
             </thead>
@@ -221,17 +236,9 @@ if (config::byKey('autoDiscoverEqLogic', 'myhome', 0) == 1) {
 
             </tbody>
         </table>
-
-        <form class="form-horizontal">
-            <fieldset>
-                <div class="form-actions">
-                    <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> Supprimer</a>
-                    <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> Sauvegarder</a>
-                </div>
-            </fieldset>
-        </form>
-
-    </div>
+		</div>
+	</div>
+	</div>
 </div>
 
 <?php include_file('desktop', 'myhome', 'js', 'myhome'); ?>
