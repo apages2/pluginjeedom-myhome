accc277d97979fe58c0dd1f2f8867ee0a4a9af1b
root <root@raspberrypi>
2019-11-17 08:02:58 +0000

    php7.3 et jeedom V4

--- desktop/php/myhome.php
+++ desktop/php/myhome.php
@@ -141,7 +141,7 @@ if (config::byKey('autoDiscoverEqLogic', 'myhome', 0) == 1) {
                                 <select class="eqLogicAttr form-control" data-l1key="object_id">
                                     <option value="">Aucun</option>
                                     <?php
-                                    foreach (object::all() as $object) {
+                                    foreach (jeeObject::all() as $object) {
                                         echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
                                     }
                                     ?>
